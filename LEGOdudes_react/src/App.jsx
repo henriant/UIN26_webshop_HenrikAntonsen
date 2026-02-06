3d6592ae368eb130324136aecb761f707adba622
henriant <henant@outlook.com>
2026-02-06 13:57:24 +0100

    Forelesning

--- LEGOdudes_react/src/App.jsx
+++ LEGOdudes_react/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { products } from './assets/legodudes'
 import './style/lego.css'
 function App() {
 
   const [isOpen, setIsOpen] = useState(false)
+  const [cart, setCart] = useState([])
+  const [cartQuantity, setCartQuantity] = useState(0)
+
+  console.log("cart", cart)
+  useEffect(() => {
+    const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0)
+    setCartQuantity(totalQuantity)
+  }, [cart])
 
 
-function Header({setIsOpen}){
+function Header({setIsOpen, cartQuantity}){
   return(
     <header>
       <h1>
@@ -15,7 +23,7 @@ function Header({setIsOpen}){
         </a>
       </h1>
       <button id="cart-button" onClick={() => setIsOpen((prev) => !prev)}>
-        <div id="cart-quantity">0</div>
+        <div id="cart-quantity">{cartQuantity}</div>
         <img src="website_images/legocart.svg" alt="Handlevogn" />
       </button>
     </header>
@@ -39,15 +47,20 @@ function CategoryTitle(){
   return(<h2>Ninjago</h2>)
 }
 
-function Products({products}){
+function Products({products, setCart}){
   return(
   <div id="product-list">
-    {products.map(p => <ProductCard key={p.prodid} p={p} />)}
+    {products.map(p => <ProductCard key={p.prodid} p={p} setCart={setCart}/>)}
   </div>)
 }
 
-function ProductCard({p}){
+function ProductCard({p, setCart}){
+
   const handleClick = () => {
+    setCart((prev) => 
+      prev.some(item => item.prodid === p.prodid) ? 
+      prev.map(item => item.prodid === p.prodid ? {...item, quantity: item.quantity + 1}
+      : item ) : [...prev, {...p, quantity: 1}])
     console.log("Legg i handlevogn")
   }
 
@@ -57,48 +70,57 @@ function ProductCard({p}){
     <article className="product-card">
         <img src={`website_images/PROD_${p.imagefile}`} alt={p.title} />
         <a href="#">${p.category}</a>
-        <h3>${p.title}</h3>
-        <p>Kr. ${p.price},-</p>
+        <h3>{p.title}</h3>
+        <p>Kr. {p.price},-</p>
         <button onClick={handleClick}>Legg til handlevogn</button>
     </article>
   )
 }
 
-function Cart({isOpen}) {
+function Cart({isOpen, cart, setCart}) {
     return(
       <section id="cart" className={isOpen ? "" : "hidden"}>
         <table id="cart-items">
           <tbody>
-            <tr>
+            {cart.length < 0 ?
+            (<tr>
             <td>Ingen varer i handlevognen enda.</td>
-            </tr>
+            </tr>) : (cart.map(p => <CartItem key={p.prodid} p={p} setCart={setCart}/> ))
+            }
           </tbody>
         </table>
-          <p>Total pris: <span id="total-price">0</span>NOK</p>
+          <p>Total pris: <span id="total-price">0</span> NOK</p>
       </section>
     )
   }
 
-  function CartItem() {
+  function CartItem({p, setCart}) {
+
+    const removeFromCart = (prodid) =>{
+      setCart(prev => prev.map(item => item.prodid === prodid ? {...item, quantity: item.quantity - 1}: item)
+      .filter(item => item.quantity > 0)
+      )
+    }
+
     return(
       <tr>
-        <td className="title">${product.title}</td>
-        <td className="price">${product.price}</td>
-        <td className="quantity">${ci.quantity}</td>
-        <td className="delete"><button onClick="deleteFromCart(${product.prodid})">X</button></td>
+        <td className="title">{p.title}</td>
+        <td className="price">{p.price}</td>
+        <td className="quantity">{p.quantity}</td>
+        <td className="delete"><button onClick={()=>removeFromCart(p.prodid)}>X</button></td>
       </tr>
     )
   }
 
   return (
     <div id="container">
-      <Header setIsOpen={setIsOpen} />
+      <Header setIsOpen={setIsOpen} cartQuantity={cartQuantity}/>
       <Nav />
       <main>
         <CategoryTitle />
-        <Products products={products} />
+        <Products products={products} setCart={setCart} />
       </main>
-      <Cart isOpen={isOpen}/>
+      <Cart isOpen={isOpen} cart={cart} setCart={setCart}/>
       
 
     </div>
