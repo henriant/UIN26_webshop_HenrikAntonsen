9814369af65a4b6bc2d47683c7864090293c8689
henriant <henant@outlook.com>
2026-02-16 13:58:12 +0100

    Forelesning
    Pokemon API

--- Routing/src/components/CategoryLayout.jsx
+++ Routing/src/components/CategoryLayout.jsx
@@ -1,14 +1,28 @@
 import { Outlet, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function CategoryLayout() {
+
+    const [apiData, setApiData] = useState([])
+    const [apiEndpoint, setApiEndpoint] = useState()
+
+    const getData = async()=>{
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10')
+        const data = await response.json()
+        setApiData(data.results)
+    }
+
+    console.log(apiData)
+    console.log(apiEndpoint)
+
+    useEffect(()=>{
+        getData()
+    }, [])
+
     return (
         <>
-        <nav>
-            <Link to="sko">Sko</Link>
-            <Link to="bukse">Bukse</Link>
-            <Link to="svampebobsokker">Svampebobsokker</Link>
-            <Link to="svampebobgenser">Svampebobgenser</Link>
-            <Link to="svampebobundertøy">Svampebobundertøy</Link>
+        <nav className='main-nav'>
+            {apiData?.map((item) => <Link key={item.name + '-xt'} to={item.name} onClick={() => setApiEndpoint(item.url)}>{item.name}</Link>)}
         </nav>
         <Outlet />
         </>
